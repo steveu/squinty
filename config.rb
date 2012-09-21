4d6517240539f5d1690ce007b28a9994f514b8de
Steve Urmston <steve@urm.st>
2012-09-21 13:13:52 +0100

    Chrome only CSS

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "app/css"
+css_dir = "extension/css"
 sass_dir = "sass"
-images_dir = "app/img"
-javascripts_dir = "app/js"
+images_dir = "extension/img"
+javascripts_dir = "extension/js"
 
 
 # You can select your preferred output style here (can be overridden via the command line):
